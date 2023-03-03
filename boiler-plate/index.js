aa4a328022d0918d9d43f65f8f33b02d74615395
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-03 22:17:24 +0900

    Update express

---
+++ boiler-plate/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+//----------------------------------------------------------------------------
+// 다운받은 express 불러오기 = ES6 문법의 import 시스템
+// 새로운 express app을 만든다
+// port 지정(백서버 지정)
+
+// "/" : root directory
+// app에 get 요청 => Hello Woeld! 출력
+// 5000port에서 실행
